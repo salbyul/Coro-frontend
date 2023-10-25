290965bd778e7c6b2b9d193b066109405103ca7a
salbyul <kpast96@gmail.com>
2023-10-25 19:14:39 +0900

    refactor: 404 not found page

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { hasToken } from './composables/useHandlingToken';
 import Header from './components/layouts/Header.vue';
 function checkToken() {
     const currentPath = window.location.pathname;
-    const notRequiredPath = ['/login', '/registration'];
+    const notRequiredPath = ['/login', '/registration', '/404'];
     const isRequired =
         notRequiredPath.filter((path) => path === currentPath).length === 0;
     if (!hasToken() && isRequired) {
