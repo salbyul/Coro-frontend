d70c138bcbd87481f3c63277b1e18ee8828d972b
salbyul <kpast96@gmail.com>
2023-11-02 13:28:52 +0900

    delete port

--- src/api/http.js
+++ src/api/http.js
@@ -12,7 +12,7 @@ function createInstance(baseURL, options) {
     return axios.create(Object.assign({ baseURL: baseURL }, options));
 }
 
-export const http = createInstance('http://localhost:8080/api');
+export const http = createInstance('http://localhost/api');
 
 // 토큰이 존재하면 헤더에 포함시킨다.
 http.interceptors.request.use((config) => {
