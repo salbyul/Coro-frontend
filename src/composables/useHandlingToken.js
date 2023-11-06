3ebc202a5a3a6342a552a10bb3f7109366f0528d
salbyul <kpast96@gmail.com>
2023-11-06 17:28:07 +0900

    change setting about cookies

--- src/composables/useHandlingToken.js
+++ src/composables/useHandlingToken.js
@@ -2,8 +2,8 @@ import { useCookies } from 'vue3-cookies';
 const { cookies } = useCookies();
 
 export const setToken = (accessToken, refreshToken) => {
-    cookies.set('accessToken', accessToken, '1d', null, null, true);
-    cookies.set('refreshToken', refreshToken, '1d', null, null, true);
+    cookies.set('accessToken', accessToken, '1d', '/', null, false);
+    cookies.set('refreshToken', refreshToken, '1d', '/', null, false);
 };
 
 export const hasToken = () => {
